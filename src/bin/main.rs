92d8816a81b0d06ac8d48b5e9533947b487f7a79
funnimonkedev <113639481+funnimonkedev@users.noreply.github.com>
2023-05-13 09:19:56 -0400

    Update main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,38 +1,42 @@
 use std::io;
 use loxable::Lexer;
-// use std::{fs, io, io::Read};
+use std::{fs, io::Read};
 
 fn main() {
-    /*
-    println!("welcome to loxable shell, please input file name.");
-    let mut filename = String::new();
-    io::stdin().read_line(&mut filename).expect("not a valid input. (please dont use emojis or etc)");
-    let filename = filename.trim();
-    let mut args = String::new();
-    let mut file = fs::File::open(filename).unwrap();
-    file.read_to_string(&mut args).unwrap();
-    dbg!(file);
-    let args = args.trim();
-    // println!("would you like to use untested/unstable version? (y/n)   \n");
-    println!("{}",args);
+    
+    println!("Would you like to provide a terminal based input or file based input? type `1` for terminal, or `2` for file.");
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input).expect("failed to read user input.");
+    let user_input = user_input.trim();
+    if user_input == "2" {
+        println!("\nWelcome to loxable cargo, please input filename.");    
+        let mut filename = String::new();
+        io::stdin().read_line(&mut filename).expect("not a valid input. (please dont use emojis or etc)");
+        let filename = filename.trim();
+        let mut args = String::new();
+        let mut file = fs::File::open(filename).unwrap();
+        file.read_to_string(&mut args).unwrap();
+        dbg!(file);
+        let input = args.trim();
 
-    if args.starts_with("loxabla beta/") {
-        println!("version type declared succesfully.");
-    } else if !args.starts_with("loxable beta/") {
-        panic!("current version of loxable is unavailable in this version, please upgrade your loxable version.");
-    }
-    */
-    let mut i = 0;
-    loop {
-        println!("\nwelcome to loxable shell, alpha version. commands executed so far: {}",i);
-
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("not a valid ascii string");
-        let input = input.trim();
         let input: Vec<char> = input.chars().collect();
-        let mut to_lex = Lexer { text: input, pos: 0, current_char: ' '  };
+        let mut to_lex = Lexer { text: input, pos: 0, current_char: ' ' };
         let to_lex2 = to_lex.make_tokens();
-        println!("{:#?}",to_lex2);
-        i += 1;
+        println!("{:?}",to_lex2);
+    } else if user_input == "1" {
+        let mut i = 0;
+        loop {
+            println!("\nWelcome to loxable shell, alpha version. commands executed so far: {}",i);
+
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).expect("not a valid ascii string");
+            let input = input.trim();
+            let input: Vec<char> = input.chars().collect();
+            let mut to_lex = Lexer { text: input, pos: 0, current_char: ' '  };
+            let to_lex2 = to_lex.make_tokens();
+            println!("{:?}",to_lex2);
+            i += 1;
+            println!("\n press ctrl+c to stop, or cmd+c to stop program");
+        }
     }
 }
