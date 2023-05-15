4cf53e7a3b20b73fbefac84c53a4a7f3719662ca
funnimonkedev <113639481+funnimonkedev@users.noreply.github.com>
2023-05-15 08:21:47 -0400

    Delete README.m

--- README.m
+++
@@ -1,9 +0,0 @@
-# Holy-cobol
-the Holy-cobol programming language.
-
-> "Go to the lib.rs file for the juicy dip!"
-  
-## the idea is to make it compile into cobol (it's a superset of cobol!)
-  
-  
-![Holy-cobol](https://github.com/funnimonkedev/loxable/assets/113639481/b6a6efe2-0992-4c35-8f41-996b94ce642c)
